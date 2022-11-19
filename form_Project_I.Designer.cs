922b50103153776014287c126ff88bf7f3dc141f
doannguyenfc02 <doannguyenfc02@gmail.com>
2022-11-19 23:22:53 +0700

    first commit

---
+++ form_Project_I.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace Project_I
+{
+    partial class form_Project_I
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label3;
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.labelImage = new System.Windows.Forms.Label();
+            this.Information = new System.Windows.Forms.GroupBox();
+            this.lblK = new System.Windows.Forms.Label();
+            this.lblClass = new System.Windows.Forms.Label();
+            this.lblMSSV = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtNote = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            this.Information.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label3.Location = new System.Drawing.Point(280, 244);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(38, 13);
+            label3.TabIndex = 10;
+            label3.Text = "Note:";
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnStart.Location = new System.Drawing.Point(-2, -5);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(43, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Red;
+            this.btnStop.Location = new System.Drawing.Point(37, -5);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(47, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Reset";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ID Card/MSSV:";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(93, 13);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(267, 20);
+            this.txtID.TabIndex = 3;
+            this.txtID.TextChanged += new System.EventHandler(this.txtID_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCheck);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtID);
+            this.groupBox1.Location = new System.Drawing.Point(12, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(471, 41);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(390, 12);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 4;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // pic1
+            // 
+            this.pic1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic1.Location = new System.Drawing.Point(12, 104);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(224, 129);
+            this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic1.TabIndex = 5;
+            this.pic1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Camera";
+            // 
+            // pic2
+            // 
+            this.pic2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic2.Location = new System.Drawing.Point(12, 260);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(224, 129);
+            this.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic2.TabIndex = 5;
+            this.pic2.TabStop = false;
+            // 
+            // labelImage
+            // 
+            this.labelImage.AutoSize = true;
+            this.labelImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelImage.Location = new System.Drawing.Point(9, 244);
+            this.labelImage.Name = "labelImage";
+            this.labelImage.Size = new System.Drawing.Size(41, 13);
+            this.labelImage.TabIndex = 7;
+            this.labelImage.Text = "Image";
+            // 
+            // Information
+            // 
+            this.Information.BackColor = System.Drawing.Color.Cyan;
+            this.Information.Controls.Add(this.lblK);
+            this.Information.Controls.Add(this.lblClass);
+            this.Information.Controls.Add(this.lblMSSV);
+            this.Information.Controls.Add(this.lblName);
+            this.Information.Location = new System.Drawing.Point(283, 104);
+            this.Information.Name = "Information";
+            this.Information.Size = new System.Drawing.Size(200, 129);
+            this.Information.TabIndex = 8;
+            this.Information.TabStop = false;
+            this.Information.Text = "Information";
+            // 
+            // lblK
+            // 
+            this.lblK.AutoSize = true;
+            this.lblK.Location = new System.Drawing.Point(10, 98);
+            this.lblK.Name = "lblK";
+            this.lblK.Size = new System.Drawing.Size(0, 13);
+            this.lblK.TabIndex = 3;
+            // 
+            // lblClass
+            // 
+            this.lblClass.AutoSize = true;
+            this.lblClass.Location = new System.Drawing.Point(10, 75);
+            this.lblClass.Name = "lblClass";
+            this.lblClass.Size = new System.Drawing.Size(0, 13);
+            this.lblClass.TabIndex = 2;
+            // 
+            // lblMSSV
+            // 
+            this.lblMSSV.AutoSize = true;
+            this.lblMSSV.Location = new System.Drawing.Point(10, 49);
+            this.lblMSSV.Name = "lblMSSV";
+            this.lblMSSV.Size = new System.Drawing.Size(0, 13);
+            this.lblMSSV.TabIndex = 1;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(7, 20);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(0, 13);
+            this.lblName.TabIndex = 0;
+            // 
+            // txtNote
+            // 
+            this.txtNote.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNote.Location = new System.Drawing.Point(282, 260);
+            this.txtNote.Name = "txtNote";
+            this.txtNote.Size = new System.Drawing.Size(201, 129);
+            this.txtNote.TabIndex = 9;
+            this.txtNote.Text = "";
+            this.txtNote.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(408, 418);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(215, 398);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(78, 43);
+            this.btnSave.TabIndex = 12;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // form_Project_I
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 450);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(label3);
+            this.Controls.Add(this.txtNote);
+            this.Controls.Add(this.Information);
+            this.Controls.Add(this.labelImage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pic2);
+            this.Controls.Add(this.pic1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "form_Project_I";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Project I";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.form_Project_I_FormClosing);
+            this.Load += new System.EventHandler(this.form_Project_I_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            this.Information.ResumeLayout(false);
+            this.Information.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.Label labelImage;
+        private System.Windows.Forms.GroupBox Information;
+        private System.Windows.Forms.Label lblClass;
+        private System.Windows.Forms.Label lblMSSV;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblK;
+        private System.Windows.Forms.RichTextBox txtNote;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ form_Project_I.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Data.SqlClient;
+using System.Runtime.CompilerServices;
+
+namespace Project_I
+{
+    public partial class form_Project_I : Form
+    {
+        public form_Project_I()
+        {
+            InitializeComponent();
+        }
+
+
+        VideoCaptureDevice videoCapture;
+        FilterInfoCollection filterInfo;
+        SqlConnection conn = null;
+        string strConn = "Server=DESKTOP-6LU74U9\\DOANNGUYEN;database=HUST;user id=sa;password=20200445";
+
+        int checkStart =0;
+        void StartCamera()
+        {
+                try
+                {
+                    filterInfo = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                    videoCapture = new VideoCaptureDevice(filterInfo[0].MonikerString);
+                    videoCapture.NewFrame += new NewFrameEventHandler(Camera_On);
+                    videoCapture.Start();
+                    checkStart = 1;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+        }
+        void resetInfo()
+        {
+            pic2.Image = null;
+            txtID.Text = null;
+            lblName.Text = null;
+            lblMSSV.Text = null;
+            lblClass.Text = null;
+            lblK.Text = null;
+        }
+        void Reset()
+        {
+            try
+            {
+                if (videoCapture != null && videoCapture.IsRunning)
+                {
+                    videoCapture.Stop();
+                    checkStart = 0;
+                }
+                resetInfo();        
+            }
+            catch (Exception)
+            {
+                return;
+            }
+        }
+        void TakeImage()
+        {
+            DateTime today=DateTime.Now;
+            string strTime=today.Year+"-"+today.Month+"-"+today.Day+"_"+today.Hour+"."+today.Minute+"."+today.Second;   
+            string fileName = @"C:\\Users\\doann\\Desktop\\ProjectI\\Example\\"+strTime+"_"+txtID.Text + ".jpg";
+            var bitmap = new Bitmap(pic2.Width, pic2.Height);
+            pic2.DrawToBitmap(bitmap, pic2.ClientRectangle);
+            System.Drawing.Imaging.ImageFormat imageFormat = null;
+            imageFormat = System.Drawing.Imaging.ImageFormat.Jpeg;
+            bitmap.Save(fileName, imageFormat);
+        }
+        private void Check()
+        {
+            if (checkStart==1)
+            {
+                    
+                    if (conn == null)
+                        conn = new SqlConnection(strConn);
+                    if (conn.State == ConnectionState.Closed)
+                        conn.Open();
+
+                    SqlCommand command = new SqlCommand();
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = "select * from Student where IdCard=" + txtID.Text + "or MSSV=" + txtID.Text;
+                    command.Connection = conn;
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.Read() == true) //có dữ liệu
+                    {
+                        lblMSSV.Text = "MSSV: " + reader.GetInt32(1) + "";
+                        lblName.Text = "Họ và tên: " + reader.GetString(2);
+                        lblClass.Text = "Lớp: " + reader.GetString(3);
+                        lblK.Text = "Khóa: " + reader.GetInt32(4);
+                        pic2.Image = pic1.Image;
+                    }
+                    else
+                    {
+                        resetInfo();
+                        MessageBox.Show("Thông tin của bạn chưa có trong CSDL"); ;
+                    }
+                    reader.Close();
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng start!");
+            }
+        }
+
+
+        private void Camera_On(object sender, NewFrameEventArgs eventArgs)
+        {
+            pic1.Image = (Bitmap)eventArgs.Frame.Clone();
+
+        }
+
+        private void form_Project_I_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StartCamera();
+        }
+
+        private void form_Project_I_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Reset();
+        }
+
+        private void txtID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            Check();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            Reset();
+            pic1.Image = null;
+            pic2.Image = null;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (videoCapture != null && videoCapture.IsRunning)
+            {
+                videoCapture.Stop();
+            }
+            Close();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            TakeImage();
+        }
+    }
+}
